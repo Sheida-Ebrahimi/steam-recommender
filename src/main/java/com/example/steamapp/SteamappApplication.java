ceff17403f783dbc83c04434b9d9f2abad2fb366
Sheida Ebrahimi <63651458+Sheida-Ebrahimi@users.noreply.github.com>
2026-06-26 15:21:16 -0400

    add redis caching

--- src/main/java/com/example/steamapp/SteamappApplication.java
+++ src/main/java/com/example/steamapp/SteamappApplication.java
@@ -2,10 +2,12 @@ package com.example.steamapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class SteamappApplication {
 
     public static void main(String[] args) {
